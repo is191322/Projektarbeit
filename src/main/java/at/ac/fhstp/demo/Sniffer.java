3e6fa625bf910d2b9c4b13db4b7ab470294d0216
is191322 <is191322@fhstp.ac.at>
2021-12-05 09:42:58 +0100

    erweiterung

--- src/main/java/at/ac/fhstp/demo/Sniffer.java
+++ src/main/java/at/ac/fhstp/demo/Sniffer.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 //Teilnehmer im Netzwerk = Besitzer
 public class Sniffer {
-    private int id;
+    private String username;
     private String name;
     private List<Update> personalupdates;
     private List<Update> sharedupdates;
+    private List<Update> timeline;
     private List<Sniffer> followedpeople;
 }
---
+++ src/main/java/at/ac/fhstp/demo/SnifferManager.java
@@ -0,0 +1,9 @@
+package at.ac.fhstp.demo;
+
+import java.util.List;
+
+//Verwaltung aller Sniffer
+public class SnifferManager {
+    private List<Sniffer> allsniffers;
+    
+}
