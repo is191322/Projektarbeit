1fbcdb55b12850f24bd61df8e007bd622280acb8
is191322 <is191322@fhstp.ac.at>
2021-12-08 11:14:40 +0100

    FollowersService+ Controller

--- src/main/java/at/ac/fhstp/demo/FollowerRepository.java
+++ src/main/java/at/ac/fhstp/demo/FollowerRepository.java
@@ -1,9 +1,12 @@
 package at.ac.fhstp.demo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 //Verwaltet alle Pupdates
 //Singelton
-public interface FollowerRepository extends CrudRepository<FollowerEntity, Integer> {
-
+public interface FollowerRepository extends JpaRepository<FollowerEntity, Integer> {
+    List<FollowerEntity> findFollowerstoSniffer(int snifferID);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-//INSERT INTO pupdates(id, UserID, Text) VALUES (1, 1, "First Post");
-//INSERT INTO sniffers(id, username, name) VALUES (1, "is191325", "Philipp Zöchbauer");
-//INSERT INTO sniffers(id, username, name) VALUES (1, "is191322", "Christopher Fritz");
+/*
+//INSERT INTO SNIFFERS VALUES (1, 'Christopher Fritz', 'is191322');
+INSERT INTO SNIFFERS VALUES (2, 'Philipp Zöchbauer', 'is191325');
+INSERT INTO pupdates(id, UserID, Text) VALUES (1, 1, 'First Post');
+*/
