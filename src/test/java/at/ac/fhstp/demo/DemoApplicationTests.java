81fdeb30b59557084f964f8bd91bf1a6c525485b
is191322 <is191322@fhstp.ac.at>
2021-12-11 11:24:34 +0100

    Start

--- src/test/java/at/ac/fhstp/demo/DemoApplicationTests.java
+++ src/test/java/at/ac/fhstp/demo/DemoApplicationTests.java
@@ -1,13 +1,35 @@
 package at.ac.fhstp.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Mock 
+	PupdateEntity pupdate;
+	@Mock
+	PupdateRepository pupdateRepository;
 	@Test
 	void contextLoads() {
+	}
+	
+
+	@Test
+	public void testSaveSniffers(){
+		assertNotNull(pupdate);
+		
+		List<PupdateEntity> pupdates =pupdateRepository.findPupdatesbySniffer(1);
+		assertNotNull(pupdates);
 	}
 
 }
